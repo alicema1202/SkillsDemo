d6594a333cdc9c74c520f85dd3f40de3001d05a6
alicema1202 <64507532+alicema1202@users.noreply.github.com>
2022-05-01 16:27:54 -0700

    added code!

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,3 +1,15 @@
 public class ReverseArray {
-//continue in VS code!!
+    public static void reverseArray(int[] input, int index1, int index2) {
+        int[] reversed = new int[index2 - index1 + 1];
+        int index = 0;
+        for (int i = index2; i > index1; i--) {
+            reversed[index] = input[i];
+            index++;
+        }
+        index = 0;
+        for (int i = index2; i <= index1; i++) {
+            input[i] = reversed[index];
+            index++;
+        }
+    }
 }
---
+++ ReverseArrayTest.java
@@ -0,0 +1,19 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ReverseArrayTest {
+    private int[] fullList;
+    @Before
+    public void setUp() {
+        fullList = new int[] {1,2,3,4,5};
+    }
+
+    @Test
+    public void testReverse() {
+        ReverseArray.reverseArray(fullList, 1, 4);
+        int[] expected = new int[] {1,5,4,3,2};
+
+        assertArrayEquals(expected, fullList);
+    //this is a failing test!
+    }
+}
